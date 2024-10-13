ebbc8bd5c0eb468d09be1bea758c954640e41d0b
HERSpzd <49366653+HERSpzd@users.noreply.github.com>
2024-10-13 23:43:28 +0800

    add a get all fundraisers (including inactive) API)

--- A3-api/controllerAPI/api-controller.js
+++ A3-api/controllerAPI/api-controller.js
@@ -15,10 +15,30 @@ var router = express.Router();
  */
 router.get("/fundraisers", (req, res) => {
   connection.query(
-    "SELECT f.*, c.NAME AS CATEGORY_NAME " + 
-    "FROM FUNDRAISER f " + 
-    "JOIN CATEGORY c ON f.CATEGORY_ID = c.CATEGORY_ID " + 
+    "SELECT f.*, c.NAME AS CATEGORY_NAME " +
+    "FROM FUNDRAISER f " +
+    "JOIN CATEGORY c ON f.CATEGORY_ID = c.CATEGORY_ID " +
     "WHERE f.ACTIVE = true",
+    (err, records, fields) => {
+      if (err) {
+        console.error("Error while retrieve the data");
+      } else {
+        res.send(records);
+      }
+    })
+})
+
+/**
+ * 
+ * Get all fundraisers from the database
+ * 
+ */
+router.get("/all_fundraisers", (req, res) => {
+  connection.query(
+    "SELECT f.*, c.NAME AS CATEGORY_NAME " +
+    "FROM FUNDRAISER f " +
+    "JOIN CATEGORY c ON f.CATEGORY_ID = c.CATEGORY_ID " +
+    "ORDER BY FUNDRAISER_ID",
     (err, records, fields) => {
       if (err) {
         console.error("Error while retrieve the data");
@@ -56,68 +76,68 @@ router.get("/category", (req, res) => {
 router.get("/search", (req, res) => {
   // Search organizer
   if (!req.query.CITY && !req.query.CATEGORY_NAME) {
-    var query = "SELECT f.*, c.NAME AS CATEGORY_NAME " + 
-                "FROM FUNDRAISER f " + 
-                "JOIN CATEGORY c ON f.CATEGORY_ID = c.CATEGORY_ID " + 
-                "WHERE f.ACTIVE = true " +  
-                "AND f.ORGANIZER = ?";
+    var query = "SELECT f.*, c.NAME AS CATEGORY_NAME " +
+      "FROM FUNDRAISER f " +
+      "JOIN CATEGORY c ON f.CATEGORY_ID = c.CATEGORY_ID " +
+      "WHERE f.ACTIVE = true " +
+      "AND f.ORGANIZER = ?";
     var parameters = [req.query.ORGANIZER];
     // Search city
   } else if (!req.query.ORGANIZER && !req.query.CATEGORY_NAME) {
-    var query = "SELECT f.*, c.NAME AS CATEGORY_NAME " + 
-                "FROM FUNDRAISER f " + 
-                "JOIN CATEGORY c ON f.CATEGORY_ID = c.CATEGORY_ID " + 
-                "WHERE f.ACTIVE = true " + 
-                "AND f.CITY = ?";
+    var query = "SELECT f.*, c.NAME AS CATEGORY_NAME " +
+      "FROM FUNDRAISER f " +
+      "JOIN CATEGORY c ON f.CATEGORY_ID = c.CATEGORY_ID " +
+      "WHERE f.ACTIVE = true " +
+      "AND f.CITY = ?";
     var parameters = [req.query.CITY];
 
     // Search category
   } else if (!req.query.ORGANIZER && !req.query.CITY) {
-    var query = "SELECT f.*, c.NAME AS CATEGORY_NAME " + 
-                "FROM FUNDRAISER f " + 
-                "JOIN CATEGORY c ON f.CATEGORY_ID = c.CATEGORY_ID " + 
-                "WHERE f.ACTIVE = true " +  
-                "AND c.NAME = ?";
+    var query = "SELECT f.*, c.NAME AS CATEGORY_NAME " +
+      "FROM FUNDRAISER f " +
+      "JOIN CATEGORY c ON f.CATEGORY_ID = c.CATEGORY_ID " +
+      "WHERE f.ACTIVE = true " +
+      "AND c.NAME = ?";
     var parameters = [req.query.CATEGORY_NAME];
 
 
     // Search organizer, city
   } else if (!req.query.CATEGORY_NAME) {
-    var query = "SELECT f.*, c.NAME AS CATEGORY_NAME " + 
-                "FROM FUNDRAISER f " + 
-                "JOIN CATEGORY c ON f.CATEGORY_ID = c.CATEGORY_ID " + 
-                "WHERE f.ACTIVE = true " + 
-                "AND f.ORGANIZER = ? " +  
-                "AND f.CITY = ?";
+    var query = "SELECT f.*, c.NAME AS CATEGORY_NAME " +
+      "FROM FUNDRAISER f " +
+      "JOIN CATEGORY c ON f.CATEGORY_ID = c.CATEGORY_ID " +
+      "WHERE f.ACTIVE = true " +
+      "AND f.ORGANIZER = ? " +
+      "AND f.CITY = ?";
     var parameters = [req.query.ORGANIZER, req.query.CITY];
     // Search organizer, category
   } else if (!req.query.CITY) {
-    var query = "SELECT f.*, c.NAME AS CATEGORY_NAME " + 
-                "FROM FUNDRAISER f " + 
-                "JOIN CATEGORY c ON f.CATEGORY_ID = c.CATEGORY_ID " + 
-                "WHERE f.ACTIVE = true " +  
-                "AND f.ORGANIZER = ? " +  
-                "AND c.NAME = ?";
+    var query = "SELECT f.*, c.NAME AS CATEGORY_NAME " +
+      "FROM FUNDRAISER f " +
+      "JOIN CATEGORY c ON f.CATEGORY_ID = c.CATEGORY_ID " +
+      "WHERE f.ACTIVE = true " +
+      "AND f.ORGANIZER = ? " +
+      "AND c.NAME = ?";
     var parameters = [req.query.ORGANIZER, req.query.CATEGORY_NAME];
     // Search city, category
   } else if (!req.query.ORGANIZER) {
-    var query = "SELECT f.*, c.NAME AS CATEGORY_NAME " + 
-                "FROM FUNDRAISER f " + 
-                "JOIN CATEGORY c ON f.CATEGORY_ID = c.CATEGORY_ID " + 
-                "WHERE f.ACTIVE = true " + 
-                "AND f.CITY = ? " +  
-                "AND c.NAME = ?";
+    var query = "SELECT f.*, c.NAME AS CATEGORY_NAME " +
+      "FROM FUNDRAISER f " +
+      "JOIN CATEGORY c ON f.CATEGORY_ID = c.CATEGORY_ID " +
+      "WHERE f.ACTIVE = true " +
+      "AND f.CITY = ? " +
+      "AND c.NAME = ?";
     var parameters = [req.query.CITY, req.query.CATEGORY_NAME];
     // Search organizer, city and category
   } else {
 
-    var query = "SELECT f.*, c.NAME AS CATEGORY_NAME " + 
-                "FROM FUNDRAISER f " + 
-                "JOIN CATEGORY c ON f.CATEGORY_ID = c.CATEGORY_ID " + 
-                "WHERE f.ACTIVE = true " +  
-                "AND f.ORGANIZER = ? " + 
-                "AND f.CITY = ? " + 
-                "AND c.NAME = ?";
+    var query = "SELECT f.*, c.NAME AS CATEGORY_NAME " +
+      "FROM FUNDRAISER f " +
+      "JOIN CATEGORY c ON f.CATEGORY_ID = c.CATEGORY_ID " +
+      "WHERE f.ACTIVE = true " +
+      "AND f.ORGANIZER = ? " +
+      "AND f.CITY = ? " +
+      "AND c.NAME = ?";
     var parameters = [req.query.ORGANIZER, req.query.CITY, req.query.CATEGORY_NAME];
   }
 
@@ -245,5 +265,7 @@ router.delete("/delete_fundraiser/:id", (req, res) => {
   })
 
 })
+
+
 
 module.exports = router;
